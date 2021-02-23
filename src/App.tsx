ffe3b56822ad3d4ea0179f9592dcfc8258462690
Yan Garcia <yangarciavaz@gmail.com>
2021-02-23 00:17:50 -0300

    iniciando primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ExperienceBar } from './components/ExperienceBar';
+
+import './styles/global.css';
+
 function App() {
   return (
-    <h1> Iniciando aplicação </h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
